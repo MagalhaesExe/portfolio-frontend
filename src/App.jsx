d9642a57d67cacccb0e84fe360187cbe3dc44d82
MagalhaesExe <alexmagalhaesjr15@gmail.com>
2026-08-30 19:00:09 -0300

    feat: integra Contact/Footer em App.jsx e adicionar meta tags de SEO

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Hero } from './components/Hero/Hero'
 import { About } from './components/About/About'
 import { Projects } from './components/Projects/Projects'
 import { Skills } from './components/Skills/Skills'
+import { Contact } from './components/Contact/Contact'
+import { Footer } from './components/Footer/Footer'
 
 function App() {
   return (
@@ -15,8 +17,9 @@ function App() {
           <About />
           <Projects />
           <Skills />
-          {/* Contato e Footer entram aqui */}
+          <Contact />
         </main>
+        <Footer />
       </div>
     </ThemeProvider>
   )
