f3d272d127094c606b6ec7913b2ac66599997a4e
MagalhaesExe <alexmagalhaesjr15@gmail.com>
2026-08-30 18:27:50 -0300

    feat: integra todas as seções em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { ThemeProvider } from './context/ThemeContext'
 import { Header } from './components/Header/Header'
+import { Hero } from './components/Hero/Hero'
+import { About } from './components/About/About'
+import { Projects } from './components/Projects/Projects'
+import { Skills } from './components/Skills/Skills'
 
 function App() {
   return (
@@ -7,7 +11,11 @@ function App() {
       <div className="min-h-screen">
         <Header />
         <main className="pt-16">
-          {/* Hero, Sobre, Projetos, Habilidades, Contato e Footer entram aqui */}
+          <Hero />
+          <About />
+          <Projects />
+          <Skills />
+          {/* Contato e Footer entram aqui */}
         </main>
       </div>
     </ThemeProvider>
